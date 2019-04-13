13d33f5e176173d5a5a983b16b42f0c235c4a67f
Pedro Neto <pedromneto97@gmail.com>
2019-04-13 17:08:45 -0300

    Unnecessary functions

--- Domain/MongoDomain.py
+++ Domain/MongoDomain.py
@@ -1,31 +1,10 @@
-from typing import List
-
-from mongoengine import InvalidQueryError
-
-LOOKUP = 1
-UNWIND = 2
-
-
-def pipeline(operator: int = 0, **kwargs) -> dict:
-    if operator is LOOKUP:
-        if check_kwargs(['from_collection', 'local_field', 'foreign_field'], kwargs):
-            return lookup(kwargs['from_collection'], kwargs['local_field'], kwargs['foreign_field'],
-                          kwargs['local_field'] if 'as_field' not in kwargs.keys() else kwargs['as_field'])
-        raise InvalidQueryError
-    if operator is UNWIND:
-        if check_kwargs(['path', 'local_field', 'foreign_field'], kwargs):
-            return lookup(kwargs['from_collection'], kwargs['local_field'], kwargs['foreign_field'],
-                          kwargs['local_field'] if 'as_field' not in kwargs.keys() else kwargs['as_field'])
-        raise InvalidQueryError
-
-
 def lookup(from_collection: str, local_field: str, foreign_field: str, as_field: str = None):
     return {
         '$lookup': {
             'from': from_collection,
             'localField': local_field,
             'foreignField': foreign_field,
-            'as': as_field
+            'as': as_field if as_field is not None else local_field
         }
     }
 
@@ -40,10 +19,3 @@ def unwind(path: str, preserve_null_and_empty_arrays: bool = True, include_array
     if include_array_index is not None:
         uw['$unwind'].update({'includeArrayIndex': include_array_index})
     return uw
-
-
-def check_kwargs(keys: List[str], kwargs):
-    for key in keys:
-        if key not in kwargs.keys():
-            return False
-    return True
