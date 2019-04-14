e9d49e38e19bedc1fe33e7254b6155b31a08cdce
Pedro Neto <pedromneto97@gmail.com>
2019-04-14 15:09:35 -0300

    Group aggregate

--- Domain/MongoDomain.py
+++ Domain/MongoDomain.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 def lookup(from_collection: str, local_field: str, foreign_field: str = '_id', as_field: str = None) -> dict:
     return {
         '$lookup': {
@@ -19,3 +22,19 @@ def unwind(path: str, preserve_null_and_empty_arrays: bool = True, include_array
     if include_array_index is not None:
         uw['$unwind'].update({'includeArrayIndex': include_array_index})
     return uw
+
+
+# id of the group and list of tuples (field, accumulator, expression)
+def group(tuple_array: List[Tuple[str, str, str]], _id='_id') -> dict:
+    gp = {
+        '$group': {
+            '_id': _id
+        }
+    }
+    for field, accumulator, expression in tuple_array:
+        gp['$group'].update({
+            field: {
+                accumulator: expression
+            }
+        })
+    return gp
