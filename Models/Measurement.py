70ea2e51dc43aad8eafd0d81f386ae9bed4ea141
Pedro Neto <pedromneto97@gmail.com>
2019-04-19 17:21:45 -0300

    Correct name

--- Models/Measurement.py
+++ Models/Measurement.py
@@ -3,7 +3,7 @@ from mongoengine import *
 from Models.SceneItem import SceneItem
 
 
-class Measurement(document):
+class Measurement(Document):
     value = IntField(required=True)
     timestamp = DateTimeField(required=True)
     sensor = ReferenceField(SceneItem, reverse_delete_rule=DENY)
