64b15020991b8a5fc169921350e16eb682c43988
Pedro Neto <pedromneto97@gmail.com>
2019-04-19 13:06:20 -0300

    SceneItem icon not required

--- Models/SceneItem.py
+++ Models/SceneItem.py
@@ -6,5 +6,5 @@ from Models.SceneItemType import SceneItemType
 class SceneItem(Document):
     name = StringField(required=True)
     pattern = IntField(required=True, min_value=0)
-    icon = StringField(required=True)
+    icon = StringField()
     type = ReferenceField(SceneItemType, reverse_delete_rule=DENY, required=True)
