0ac8d5bf0a58c92f168e6a9f62884de37e55296e
Pedro Neto <pedromneto97@gmail.com>
2019-04-24 10:43:25 -0300

    New measurement

--- Controllers/MeasurementController.py
+++ Controllers/MeasurementController.py
@@ -1,6 +1,11 @@
 from Models.Measurement import Measurement
 
-
 def get_last_measurement(sensor: str) -> Measurement or None:
     m = Measurement.objects(sensor=sensor).order_by('-timestamp').limit(1).first()
     return m.to_json() if m is not None else None
+
+
+def new_measurement(data: str) -> Measurement:
+    m = Measurement.from_json(data)
+    m.save()
+    return m.to_json()
