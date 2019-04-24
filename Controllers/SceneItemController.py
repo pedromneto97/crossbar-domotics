e00a100d616f2e2a42e49547b8a423973a61e521
Pedro Neto <pedromneto97@gmail.com>
2019-04-24 11:36:52 -0300

    SceneItem

---
+++ Controllers/SceneItemController.py
@@ -0,0 +1,18 @@
+from Models.SceneItem import SceneItem
+
+
+def get_scene_item(_id: str) -> SceneItem or None:
+    si = SceneItem.objects(_id=_id).first()
+    return si if si is not None else None
+
+
+def insert_scene_item(data: str) -> SceneItem:
+    si = SceneItem.from_json(data)
+    si.save()
+    return si
+
+
+def edit_scene_item(_id: str, data: str):
+    si = SceneItem.objects(_id=_id).first()
+    si.from_json(data)
+    si.save()
