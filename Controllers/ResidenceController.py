f557dccefca29eb10a53c0340aa7ef60d4785465
Pedro Neto <pedromneto97@gmail.com>
2019-04-24 10:46:36 -0300

    with users

--- Controllers/ResidenceController.py
+++ Controllers/ResidenceController.py
@@ -23,7 +23,7 @@ def get_residence(_id: str) -> Residence or None:
 
 
 def get_residence_by_alias(alias: str, with_city: bool = True, with_type: bool = True,
-                           with_rooms: bool = True) -> Residence or None:
+                           with_rooms: bool = True, with_users: bool = False) -> Residence or None:
     pipeline = []
     if with_city:
         pipeline.append(lookup('address', 'address.postal_code'))
@@ -31,6 +31,8 @@ def get_residence_by_alias(alias: str, with_city: bool = True, with_type: bool =
     if with_type:
         pipeline.append(lookup('residence_type', 'type'))
         pipeline.append(unwind('$type'))
+    if with_users:
+        pipeline.append(lookup('user', 'users'))
     if with_rooms:
         pipeline.append(lookup('room', 'rooms'))
         pipeline.append(unwind('$rooms'))
@@ -44,7 +46,7 @@ def get_residence_by_alias(alias: str, with_city: bool = True, with_type: bool =
             ('rooms', '$push', '$rooms'),
             ('alias', '$first', '$alias')
         ]))
-    if with_rooms or with_city or with_type:
+    if with_rooms or with_city or with_type or with_users:
         residence = list(Residence.objects(alias=alias).aggregate(*pipeline))
         return json_util.dumps(residence[0]) if len(residence) > 0 else None
     return Residence.objects(alias=alias).first().to_json()
