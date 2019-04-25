ab2726af86f83c0e0f2dee186cb4a2ae53ea420e
Pedro Neto <pedromneto97@gmail.com>
2019-04-25 18:59:17 -0300

    Edit

--- Controllers/SceneItemController.py
+++ Controllers/SceneItemController.py
@@ -1,20 +1,24 @@
+from bson import json_util
+
 from Models.SceneItem import SceneItem
 
 
 def get_scene_item(_id: str) -> SceneItem or None:
-    si = SceneItem.objects(_id=_id).first()
+    si: SceneItem = SceneItem.objects(_id=_id).first()
     return si if si is not None else None
 
 
 def insert_scene_item(data: str) -> SceneItem:
-    si = SceneItem.from_json(data)
+    si: SceneItem = SceneItem.from_json(data)
     si.save()
     return si
 
 
 def edit_scene_item(_id: str, data: str):
-    si = SceneItem.objects(_id=_id).first()
-    si.from_json(data)
+    si: SceneItem = SceneItem.objects(_id=_id).first()
+    data: dict = json_util.loads(data)
+    for key, item in data.items():
+        setattr(si, key, item)
     si.save()
 
 
