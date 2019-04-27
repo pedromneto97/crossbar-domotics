4a9a6df7025d94e4ce7a339d7ada1cda1bb6ce63
Pedro Neto <pedromneto97@gmail.com>
2019-04-27 17:20:22 -0300

    Controller type

--- Models/ControllerType.py
+++ Models/ControllerType.py
@@ -2,5 +2,6 @@ from mongoengine import *
 
 
 class ControllerType(Document):
-    name = StringField(required=True)
+    name = StringField()
+    model = StringField(required=True)
     available_pins = ListField(IntField(), required=True)
