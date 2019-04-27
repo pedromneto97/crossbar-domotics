52779414cd1c09485ddb606f6e95ec2ae5c069ee
Pedro Neto <pedromneto97@gmail.com>
2019-04-27 18:20:04 -0300

    Map controller pins

--- Models/ControllerType.py
+++ Models/ControllerType.py
@@ -1,7 +1,12 @@
 from mongoengine import *
 
 
+class Pins(EmbeddedDocument):
+    pin = IntField(min_value=0)
+    functions = ListField(StringField())
+
+
 class ControllerType(Document):
     name = StringField()
     model = StringField(required=True)
-    available_pins = ListField(IntField(), required=True)
+    available_pins = EmbeddedDocumentListField(Pins, required=True)
