48a4bc0114d4602d0be8ea4abe9d26c208d8492d
Pedro Neto <pedromneto97@gmail.com>
2019-04-29 19:52:59 -0300

    Build

---
+++ web/precache-manifest.065e259acfc18f66e9a3ad3e7ddf4ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47e80107077136dac90f73b65740448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d6a6507bdc884034e2",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a99f012b9ea0c56acbbd",
+    "url": "/static/css/main.b1cae7ee.chunk.css"
+  },
+  {
+    "revision": "83d6a6507bdc884034e2",
+    "url": "/static/js/2.a2432635.chunk.js"
+  },
+  {
+    "revision": "a99f012b9ea0c56acbbd",
+    "url": "/static/js/main.e8dae7d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2844881ee26293fe8bab218820615c7",
+    "url": "/static/media/caecomp.f2844881.jpg"
+  }
+]);
