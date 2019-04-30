1a076c5238811dabc6697855218d683e3b626fab
Pedro Neto <pedromneto97@gmail.com>
2019-04-29 22:12:41 -0300

    Data from microcontroller

--- Controllers/MeasurementController.py
+++ Controllers/MeasurementController.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from Models.Measurement import Measurement
+
 
 def get_last_measurement(sensor: str) -> Measurement or None:
     m = Measurement.objects(sensor=sensor).order_by('-timestamp').limit(1).first()
@@ -7,5 +10,7 @@ def get_last_measurement(sensor: str) -> Measurement or None:
 
 def new_measurement(data: str) -> Measurement:
     m = Measurement.from_json(data)
+    m.timestamp = datetime(m.timestamp[0], m.timestamp[1], m.timestamp[2], m.timestamp[3], m.timestamp[4],
+                           m.timestamp[5], 0)
     m.save()
     return m.to_json()
