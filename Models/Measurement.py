bbc1d04072b38acaa927d2c7cff73a381a321544
Pedro Neto <pedromneto97@gmail.com>
2019-04-30 18:21:03 -0300

    value must be float

--- Models/Measurement.py
+++ Models/Measurement.py
@@ -4,6 +4,6 @@ from Models.SceneItem import SceneItem
 
 
 class Measurement(Document):
-    value = IntField(required=True)
+    value = FloatField(required=True)
     timestamp = DateTimeField(required=True)
     sensor = ReferenceField(SceneItem, reverse_delete_rule=DENY)
